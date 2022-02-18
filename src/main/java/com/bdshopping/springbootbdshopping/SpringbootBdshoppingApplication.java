08a819ae29acd1bacba6a3612aee90e1b6e87330
MyungsunLee <naru101@naver.com>
2022-02-18 17:34:07 +0900

    inition commit

---
+++ src/main/java/com/bdshopping/springbootbdshopping/SpringbootBdshoppingApplication.java
@@ -0,0 +1,13 @@
+package com.bdshopping.springbootbdshopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBdshoppingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBdshoppingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bdshopping/springbootbdshopping/SpringbootBdshoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdshopping.springbootbdshopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBdshoppingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
