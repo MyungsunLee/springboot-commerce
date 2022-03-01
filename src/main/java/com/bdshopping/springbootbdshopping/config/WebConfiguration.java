fb3f0c6ff6bcc25b8f851e9968511aaa726705e3
js0507dev <js0507dev@gmail.com>
2022-03-01 20:58:59 +0900

    feat: SHOP-22 CORS 설정 추가

---
+++ src/main/java/com/bdshopping/springbootbdshopping/config/WebConfiguration.java
@@ -0,0 +1,19 @@
+package com.bdshopping.springbootbdshopping.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Web 전반적인 설정
+ */
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .maxAge(1000);
+    }
+}
